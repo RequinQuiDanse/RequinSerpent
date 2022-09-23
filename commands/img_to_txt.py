18f73364326770e6cd854eb4580751634c2d8dc1
RequinQuiDanse <adam.hakim.game1908@gmail.com>
2022-09-23 18:55:21 +0200

    linux

--- commands/img_to_txt.py
+++ commands/img_to_txt.py
@@ -6,19 +6,19 @@ from pytesseract import pytesseract
   
 # Defining paths to tesseract.exe
 # and the image we would be using
-path_to_tesseract = r"J:\Applications légères\Tessera\tesseract.exe"
+path_to_tesseract = r"/usr/bin/tesseract"
 
 @bot.tree.command(name='img_to_txt', guild=discord.Object(id=769911179547246592))
 async def img_to_txt(interaction: discord.Interaction, img: discord.Attachment):
         await interaction.response.defer()
         # Opening the image & storing it in an image object
-        await img.save(r"csv_files\translate.jpg")
+        await img.save(r"csv_files/translate.jpg")
         # location to pytesseract library
         pytesseract.tesseract_cmd = path_to_tesseract
             
         # Passing the image object to image_to_string() function
         # This function will extract the text from the image
-        text = pytesseract.image_to_string(r"csv_files\translate.jpg")
+        text = pytesseract.image_to_string(r"csv_files/translate.jpg")
             
         # Displaying the extracted text
         #translateResult = translator.translate(text[:-1], dest = 'fr')
