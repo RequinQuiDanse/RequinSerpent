b4d283ec14ece36c5384208cf183783bfa3eca20
RequinQuiDanse <adam.hakim.game1908@gmail.com>
2023-09-16 13:24:40 +0200

    en

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from bot import bot, discord
 import asyncio
 import glob
 import random
+import os
 
 import commands.playlist
 import commands.crack
@@ -23,20 +24,22 @@ import commands.img_to_txt
 # import commands.meteo
 import commands.rotate_picture
 import commands.ultimate_team
-# import commands.ia_img
 import commands.gpt
 import commands.puissance4
 import commands.rap_citation
 import commands.joueurs_commentaires
 import commands.general_quizz
-# import commands.not_commands.fool
-# import commands.not_commands.embed
-# import commands.not_commands.ultimate_quizz
-# import commands.not_commands.ultimate
-# import commands.not_commands.parle
-# import commands.not_commands.vids
-# import commands.not_commands.supp
-# import commands.not_commands.git
+
+if os.name =="nt":
+    import commands.ia_img
+    import commands.not_commands.fool
+    import commands.not_commands.embed
+    import commands.not_commands.ultimate_quizz
+    import commands.not_commands.ultimate
+    import commands.not_commands.parle
+    import commands.not_commands.vids
+    import commands.not_commands.supp
+    import commands.not_commands.git
 
 @bot.event
 async def on_ready():
